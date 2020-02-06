c7345ed8849b21c6ec8709d4a11f7c1ecfff12d2
JavierTapia96 <javier.tapia@epn.edu.ec>
2020-02-06 15:56:20 -0500

    Validaciones de campos de registro

--- PetProApp/app/src/main/java/com/moviles/petproapp/RegistryActivity.kt
+++ PetProApp/app/src/main/java/com/moviles/petproapp/RegistryActivity.kt
@@ -1,56 +1,88 @@
 package com.moviles.petproapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Patterns
+import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.regex.Pattern
+
 
 class RegistryActivity : AppCompatActivity() {
+
+    private lateinit var nombreUsuario: EditText
+    private lateinit var apellidoUsuario: EditText
+    private lateinit var emailUsuario: EditText
+    private lateinit var telefonoUsuario: EditText
+    private lateinit var passwordUsuario: EditText
+    private lateinit var confirmarPasswordUsuario: EditText
+    private lateinit var db: FirebaseFirestore
+    private lateinit var nombre: String
+    private lateinit var apellido: String
+    private lateinit var email: String
+    private lateinit var telefono: String
+    private lateinit var password: String
+    private lateinit var confirmacion: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registry)
 
+        nombreUsuario = findViewById(R.id.editTextNombreReg)
+        apellidoUsuario = findViewById(R.id.editTextApellidoReg)
+        emailUsuario = findViewById(R.id.editTextEmailReg)
+        telefonoUsuario = findViewById(R.id.editTextPhoneReg)
+        passwordUsuario = findViewById(R.id.editTextPwReg)
+        confirmarPasswordUsuario = findViewById(R.id.editTextConfirmaPwReg)
+    }
 
-        val buttonAceptarRegistro: Button = findViewById(R.id.buttonAceptarRegistro)
-        buttonAceptarRegistro.setOnClickListener {
+    fun onClickRegistrarUsuario(view: View){
 
-            var nombre: EditText = findViewById(R.id.editTextNombreReg)
-            var apellido: EditText = findViewById(R.id.editTextApellidoReg)
-            var email: EditText = findViewById(R.id.editTextEmailReg)
-            var phone: EditText = findViewById(R.id.editTextPhoneReg)
-            var pw: EditText = findViewById(R.id.editTextPwReg)
-            var confPw: EditText = findViewById(R.id.editTextConfirmaPwReg)
+        nombre = nombreUsuario.text.toString()
+        apellido = apellidoUsuario.text.toString()
+        email = emailUsuario.text.toString()
+        telefono = telefonoUsuario.text.toString()
+        password = passwordUsuario.text.toString()
+        confirmacion = confirmarPasswordUsuario.text.toString()
 
-            if (nombre.text.toString().isEmpty()) {
-                nombre.setError("Ingresar nombre de usuario ")
+        when{
+            nombre.isEmpty() -> {
+                nombreUsuario.error = "Campo obligatorio"
             }
-
-            if (apellido.text.toString().isEmpty()) {
-                apellido.setError("Ingresar apellido de usuario ")
+            apellido.isEmpty() -> {
+                apellidoUsuario.error = "Campo obligatorio"
             }
-
-            if (email.text.toString().isEmpty()) {
-                email.setError("Ingresar correo electrónico de usuario ")
+            email.isEmpty() -> {
+                emailUsuario.error = "Campo obligatorio"
             }
-
-            if (phone.text.toString().isEmpty()) {
-                phone.setError("Ingresar teléfono de usuario ")
+            !validarEmail(email) -> {
+                emailUsuario.error = "Correo electrónico no valido"
             }
-
-            if (pw.text.toString().isEmpty()) {
-                pw.setError("Ingresar contraseña de usuario ")
+            telefono.isEmpty() -> {
+                telefonoUsuario.error = "Campo obligatorio"
             }
-
-            if (confPw.text.toString().isEmpty()) {
-                confPw.setError("Confirmar contraseña de usuario ")
-            } else {
-                /*val prIntent: Intent = Intent(this, Register::class.java)
-                startActivity(prIntent)*/
+            password.isEmpty() -> {
+                passwordUsuario.error = "Campo obligatorio"
             }
+            password.length < 8 -> {
+                passwordUsuario.error = "La contraseña debe tener al menos 8 caracteres"
+            }
+            confirmacion.isEmpty() -> {
+                confirmarPasswordUsuario.error = "Campo obligatorio"
+            }
+            !confirmacion.equals(password) -> {
+                confirmarPasswordUsuario.error = "La confirmacion no coincide con la contraseña"
+            }
+            else -> {
 
-
+                // Intent y funcion de validacion con email
+            }
         }
+    }
+
+    private fun validarEmail(email: String): Boolean {
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
     }
 }
