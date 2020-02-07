38294bdbbc39517216cca753e0c132362eae1c80
JavierTapia96 <javier.tapia@epn.edu.ec>
2020-02-07 15:02:45 -0500

    LoginActivity con autenticacion de usuario - Intent a home (Falta hacer home)

--- PetProApp/app/src/main/java/com/moviles/petproapp/LoginActivity.kt
+++ PetProApp/app/src/main/java/com/moviles/petproapp/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,6 +14,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var passwordUsuario: EditText
     private lateinit var usuario: String
     private lateinit var password: String
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,8 @@ class LoginActivity : AppCompatActivity() {
 
         nombreUsuario = findViewById(R.id.editTextUserLogin)
         passwordUsuario = findViewById(R.id.editTextPwLogin)
+
+        auth = FirebaseAuth.getInstance()
     }
 
 
@@ -31,22 +36,33 @@ class LoginActivity : AppCompatActivity() {
             usuario.isEmpty() -> {
                 nombreUsuario.error = "Campo obligatorio"
             }
-
             password.isEmpty() -> {
                 passwordUsuario.error = "Campo obligatorio"
             }
             password.length < 8 -> {
                 passwordUsuario.error = "La contraseña debe tener al menos 8 caracteres"
             }
-
-        else -> {
-
-        }
+            else -> {
+                loginUser()
+            }
         }
     }
 
     fun onClickButtonRegistro(view: View) {
         val prIntent: Intent = Intent(this, RegistryActivity::class.java)
         startActivity(prIntent)
+    }
+
+    private fun loginUser() {
+        auth.signInWithEmailAndPassword(usuario,password)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    val intentHome = Intent(this,HomeActivity::class.java)
+                    startActivity(intentHome)
+                    finish()
+                }else{
+                    Toast.makeText(this, "Error durante la autenticación", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
