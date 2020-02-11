66d5926936a8560b16dc61d78f4f63b524ef5a95
BryanVizuete17 <45468285+bryanvizuete17@users.noreply.github.com>
2020-02-10 23:07:05 -0500

    Declaracion de una nueva actividad Home Activity fun login User fun onClickLogin

---
+++ PetProSitterApp/app/src/main/java/com/vizuete/petprositterapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.vizuete.petprositterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- PetProSitterApp/app/src/main/java/com/vizuete/petprositterapp/LoginActivity.kt
+++ PetProSitterApp/app/src/main/java/com/vizuete/petprositterapp/LoginActivity.kt
@@ -4,17 +4,78 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
+    // declaracion de variables
+    private lateinit var nombreUsuario: EditText
+    private lateinit var passwordUsuario: EditText
+    private lateinit var usuario: String
+    private lateinit var password: String
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        nombreUsuario = findViewById(R.id.editTextUserLogin)
+        passwordUsuario = findViewById(R.id.editTextPwLogin)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun onClickLogin(view: View) {
+
+        usuario = nombreUsuario.text.toString()
+        password = passwordUsuario.text.toString()
+
+        when {
+            usuario.isEmpty() -> {
+                nombreUsuario.error = "Campo obligatorio"
+            }
+            password.isEmpty() -> {
+                passwordUsuario.error = "Campo obligatorio"
+            }
+            password.length < 8 -> {
+                passwordUsuario.error = "La contraseña debe tener al menos 8 caracteres"
+            }
+            else -> {
+                loginUser()
+            }
+        }
     }
 
     fun onClickButtonRegistro(view: View) {
         val prIntent = Intent(this, RegistryActivity::class.java)
         startActivity(prIntent)
     }
-    fun onClickLogin(view: View) {}
+
+    private fun loginUser() {
+        auth.signInWithEmailAndPassword(usuario, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    var user = auth.currentUser
+                    if (user!!.isEmailVerified) {
+                        val intentHome = Intent(this, HomeActivity::class.java)
+                        startActivity(intentHome)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Verificar su cuenta para poder iniciar sesion",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        "El usuario o contraseña son incorrectas",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 }
