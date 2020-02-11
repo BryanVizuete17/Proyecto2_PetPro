afb6c47ed9a5869766b66b6ceec3709124efb0b6
BryanVizuete17 <45468285+bryanvizuete17@users.noreply.github.com>
2020-02-10 21:10:27 -0500

    Creacion de la segunda apliacion PetProSitter Instalacion de dependencias firebase Creaccion de ActiviyLogin

---
+++ PetProSitterApp/app/src/main/java/com/vizuete/petprositterapp/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.vizuete.petprositterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onClickButtonRegistro(view: View) {}
+    fun onClickLogin(view: View) {}
+}
